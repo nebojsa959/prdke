2a28e39567bbc4e9e479c2e25a90c0acc4d7098c
nebojsa959 <nebojsa.m.93123@gmail.com>
2021-03-14 15:05:14 +0100

    Initial commit

---
+++ src/main/java/dke/prdke/Test.java
@@ -0,0 +1,65 @@
+package dke.prdke;
+
+import java.sql.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "dkepr");
+            con.setAutoCommit(false);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (Exception e) {
+            System.out.println("Failed to connect!");
+            System.out.println(e.getMessage());
+        }
+
+        String inst = "INSERT INTO test (row_id, bezeichnung) " + " VALUES (?, ?)";
+
+        // we nest every transaction in a try-block to handle exceptions and transaction management
+        try (PreparedStatement insert = con.prepareStatement(inst)) {
+            insert.setInt(1, 11111);
+            insert.setString(2, "test2");
+            insert.executeUpdate();
+
+            con.commit();
+            System.out.println("Insertion Successful");
+        } catch (SQLException ex) {
+            System.out.println("Insertion Failed");
+            ex.printStackTrace();
+            try {
+                System.out.println("Rolling back ...");
+                con.rollback();
+            } catch (SQLException ignore) {
+                System.out.println("Rollback failed - report and ignore");
+                ex.printStackTrace();
+            }
+        }
+
+        PreparedStatement stmt = null;
+        String query = "SELECT * FROM test";
+
+        try {
+            stmt = con.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            System.out.println("\nData:");
+
+            while (rs.next()) {
+                String row_id = rs.getString("row_id");
+                String bezeichnung = rs.getString("bezeichnung");
+                System.out.println(row_id + "  " + bezeichnung);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+                con.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
